86fe1d1d9fc6bd3100cdfbb439c0e00bdcbb5f17
Rschwedersky <cambara.ro@gmail.com>
2024-05-16 15:55:56 -0300

    service acc .env

--- app/page.tsx
+++ app/page.tsx
@@ -6,31 +6,14 @@ import LastMqtt from "@/components/last-mqtt";
 
 
 export default async function Home() {
-  let mqttData: Sensor_data|undefined = undefined;
-  
-  const mqttDataResponse = await fetch(`${process.env.API_URL}/api/mqtt`, {
-      headers: {
-          Authorization: `Bearer `,
-      },
-  });
-  if (mqttDataResponse.ok) {
-      const responseClone = mqttDataResponse.clone();
-      const mqttDataJson = await responseClone.json();
-      mqttData = mqttDataJson;
-      
-  }
-  
+
+
   return (
     <div>
-    {!mqttData ? ( // Check for empty object
-      <p>Loading data...</p>
-    ) : (
-      <>
-        {/* Render your content using mqttData */}
-        <h2>Received data:</h2>
+    
         <LastMqtt ></LastMqtt>
-      </>
-    )}
+   
+  
   </div>
   );
 }
