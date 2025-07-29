981838930ca6ffa49ccb68057c6902184aca8347
Rschwedersky <cambara.ro@gmail.com>
2025-07-29 14:09:47 -0300

    use service.json

--- firebase/server.ts
+++ firebase/server.ts
@@ -1,33 +1,34 @@
 import {
-    ServiceAccount,
-    cert,
-    getApps,
-    initializeApp,
+  ServiceAccount,
+  cert,
+  getApps,
+  initializeApp,
 } from "firebase-admin/app";
 import { Firestore, getFirestore } from "firebase-admin/firestore";
 import { Auth, getAuth } from "firebase-admin/auth";
+import serviceAccount from "./serviceAccount.json"; // Adjust path if needed
 
 let firestore: Firestore | undefined = undefined;
 let auth: Auth | undefined = undefined;
 
 const currentApps = getApps();
 if (currentApps.length <= 0) {
-    if (process.env.NEXT_PUBLIC_APP_ENV === "emulator") {
-        process.env["FIRESTORE_EMULATOR_HOST"] =
-            process.env.NEXT_PUBLIC_EMULATOR_FIRESTORE_PATH;
-        process.env["FIREBASE_AUTH_EMULATOR_HOST"] =
-            process.env.NEXT_PUBLIC_EMULATOR_AUTH_PATH;
-    }
-    const serviceAccount=JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG);
-    const app = initializeApp({
-        credential: cert(serviceAccount as ServiceAccount),
-    });
+  if (process.env.NEXT_PUBLIC_APP_ENV === "emulator") {
+    process.env["FIRESTORE_EMULATOR_HOST"] =
+      process.env.NEXT_PUBLIC_EMULATOR_FIRESTORE_PATH || "localhost:8080";
+    process.env["FIREBASE_AUTH_EMULATOR_HOST"] =
+      process.env.NEXT_PUBLIC_EMULATOR_AUTH_PATH || "localhost:9099";
+  }
 
-    firestore = getFirestore(app);
-    auth = getAuth(app);
+  const app = initializeApp({
+    credential: cert(serviceAccount as ServiceAccount),
+  });
+
+  firestore = getFirestore(app);
+  auth = getAuth(app);
 } else {
-    firestore = getFirestore(currentApps[0]);
-    auth = getAuth(currentApps[0]);
+  firestore = getFirestore(currentApps[0]);
+  auth = getAuth(currentApps[0]);
 }
 
 export { firestore, auth };
